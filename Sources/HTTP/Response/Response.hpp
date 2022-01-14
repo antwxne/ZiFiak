e37cf52a75aa57f2bd072b2d93a996cb8f672431
Antoine DESRUET <antoine.desruet@epitech.eu>
2022-01-15 00:20:50 +0100

    feat(HTTP): Http request and response class
    for the moment class are empty

---
+++ Sources/HTTP/Response/Response.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** Zia
+** File description:
+** Created by antoine,
+*/
+
+#ifndef ZIA_RESPONSE_HPP
+#define ZIA_RESPONSE_HPP
+
+namespace HTTP_n {
+class Response {
+};
+}
+
+#endif //ZIA_RESPONSE_HPP
