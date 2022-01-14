4e16b55253e507fdf0fe98c1395cdff4d3dcb3e7
Antoine DESRUET <antoine.desruet@epitech.eu>
2022-01-15 00:21:29 +0100

    feat(server): configuration class
    for the moment class is empty
    
    this class will contains servers
    configuration

---
+++ Sources/Server/Config/Configuration.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** Zia
+** File description:
+** Created by antoine,
+*/
+
+#include "Configuration.hpp"
---
+++ Sources/Server/Config/Configuration.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** Zia
+** File description:
+** Created by antoine,
+*/
+
+#ifndef ZIA_CONFIGURATION_HPP
+#define ZIA_CONFIGURATION_HPP
+
+namespace Server_n {
+class Configuration {
+};
+}
+
+#endif //ZIA_CONFIGURATION_HPP
