91bad55b294d4d1d1df26270a898ac85bc3e6df1
Antoine DESRUET <antoine.desruet@epitech.eu>
2022-01-23 14:08:21 +0100

    fix: compilation

--- Sources/Server.cmake
+++ Sources/Server.cmake
@@ -4,14 +4,10 @@ set(SRC
         Sources/Server/Client.cpp Sources/Server/Client.hpp
         Sources/Debug/Debug.cpp Sources/Debug/Debug.hpp
         Sources/Exceptions/MyException.cpp Sources/Exceptions/MyException.hpp
-        API/Api.cpp API/Api.hpp
         Sources/Server/Config/Configuration.cpp Sources/Server/Config/Configuration.hpp
         Sources/HTTP/Request/Request.cpp Sources/HTTP/Request/Request.hpp
         Sources/HTTP/Response/Response.cpp Sources/HTTP/Response/Response.hpp
         Sources/Watcher/Watcher.cpp Sources/Watcher/Watcher.hpp
-        API/IModule.hpp
-        API/AModule.cpp API/AModule.hpp
-        API/Context/IContext.hpp API/JSON.hpp
         )
 
 
