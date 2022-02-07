b7673ee05e0246d9bae8b0fe2d2ddb17e6ac186a
clementleplusbo <clement1.ruat@epitech.eu>
2022-02-07 16:43:18 +0100

    edit: LoadLibrary char *

--- Sources/LoadLib/LoadLib.cpp
+++ Sources/LoadLib/LoadLib.cpp
@@ -6,7 +6,7 @@
 
 #if defined(_WIN64)
 void LoadLib::dlOPenLib(const std::string &lib) {
-    auto tmp = LoadLibraryA(lib);
+    auto tmp = LoadLibraryA(lib.c_str);
     if (!tmp) {
         throw std::runtime_error(dlerror());
     _modules.push_back(tmp);
