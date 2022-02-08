787e52a8441740c801c871c7d62a54a778765f16
clementleplusbo <clement1.ruat@epitech.eu>
2022-02-08 20:04:48 +0100

    feat: directory_iterator in LoadLib

--- Sources/LoadLibs/LoadLibs.cpp
+++ Sources/LoadLibs/LoadLibs.cpp
@@ -3,7 +3,18 @@
 //
 
 #include "LoadLibs/LoadLibs.hpp"
+#include  <filesystem>
+#include <iostream>
+#include "dylib/dylib.hpp"
 
 void LoadLibs::openFilesAndStore(std::string &file) {
-    
+    std::string tmp;
+    for (auto &x: std::filesystem::directory_iterator()) {
+        try {
+            tmp = x.path();
+
+        } catch (const std::runtime_error &e) {
+            std::cerr << e.what() << std::endl;
+        }
+    };
 }
