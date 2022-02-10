c36176b567dcf1d577816f3021a171d505c69c9a
Arthur FOUQUET <arthur.fouquet@epitech.eu>
2022-02-10 16:11:06 +0100

    edit: tried to fix window compil (removed dlerror)

--- Sources/LoadLib/LoadLib.cpp
+++ Sources/LoadLib/LoadLib.cpp
@@ -8,7 +8,7 @@
 void LoadLib::dlOPenLib(const std::string lib) {
     auto tmp = LoadLibraryA(lib.c_str());
     if (!tmp) {
-        throw std::runtime_error(dlerror());
+        throw std::runtime_error("dlerror()");// dlerror() avant
     _modules.push_back(tmp);
     }
 }
