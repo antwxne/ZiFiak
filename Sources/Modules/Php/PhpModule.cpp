3ea752df70495fbff8414df205919667ad153984
Campos Theo <theo.campos@epitech.eu>
2022-02-25 19:53:22 +0100

    Edit: php cgi now return good value for the response with header and body

--- Sources/Modules/Php/PhpModule.cpp
+++ Sources/Modules/Php/PhpModule.cpp
@@ -74,20 +74,13 @@ bool zia::modules::php::PhpCgi::ShouldHandle(const ziapi::http::Context &ctx, co
     return _initSetUp;
 }
 
-void zia::modules::php::PhpCgi::Handle(ziapi::http::Context &ctx, const ziapi::http::Request &req, ziapi::http::Response &res)
+void zia::modules::php::PhpCgi::EnvSetUp(const ziapi::http::Request &req) noexcept
 {
-    std::string env = "";
-    int i = 0;
-    char buf[100];
-    res.Bootstrap();
-    res.body = "";
-
-    try {
         _env.push_back("REDIRECT_STATUS=200");
         _env.push_back("REQUEST_METHOD=" + req.method);
         _env.push_back("SCRIPT_NAME=" + req.body);
         _env.push_back("QUERY_STRING=");
-        _env.push_back("REQUEST_TIME=" + req.headers.at(ziapi::http::header::kDate));//[]);
+        _env.push_back("REQUEST_TIME=" + req.headers.at(ziapi::http::header::kDate));
         _env.push_back("REQUEST_TIME_FLOAT=" + req.headers.at(ziapi::http::header::kDate));
         _env.push_back("HTTP_ACCEPT=" + req.headers.at(ziapi::http::header::kAccept));
         //_env.push_back("HTTP_CONNECTION=" + req.headers.at(ziapi::http::header::kKeepAlive);
@@ -100,6 +93,23 @@ void zia::modules::php::PhpCgi::Handle(ziapi::http::Context &ctx, const ziapi::h
         //_env.push_back("REMOTE_ADDR=" + ctx[client.socket.address]);
         //_env.push_back("REMOTE_PORT=" + ctx[client.socket.port]);
         //_env.push_back("REMOTE_HOST=" + ctx[client.socket]);
+}
+
+void zia::modules::php::PhpCgi::Handle(ziapi::http::Context &ctx, const ziapi::http::Request &req, ziapi::http::Response &res)
+{
+    int i = 0;
+    int pos = 0;
+    int tokenPos = 0;
+    char buf[100];
+    res.body = "";
+    res.Bootstrap();
+    std::string env = "";
+    std::string resp = "";
+    std::string token;
+    std::string lilToken;
+
+    try {
+        EnvSetUp(req);
 
 #if defined(_WIN32) || defined(_WIN64)
 
@@ -128,7 +138,7 @@ void zia::modules::php::PhpCgi::Handle(ziapi::http::Context &ctx, const ziapi::h
             &pi
             )
     ) else {
-        throw runtime_error("error CreateProces");
+        throw std::runtime_error("error CreateProces");
     }
     WaitForSingleObject( pi.hProcess, INFINITE );
     CloseHandle( pi.hProcess );
@@ -139,16 +149,26 @@ void zia::modules::php::PhpCgi::Handle(ziapi::http::Context &ctx, const ziapi::h
             env += " " + _env[i];
             i++;
         }
+
         auto file = popen(("env -i " + env + "./$PATH_INFO").c_str(), 0);
 
         if (file != NULL) {
             while (std::fgets(buf, 100, file)) {
-                res.body += buf;
+                resp += buf;
             }
+            while ((pos = resp.find("\r\n")) != std::string::npos) {
+                token = resp.substr(0, pos);
+                while ((tokenPos = token.find(":")) != std::string::npos) {
+                    lilToken = token.substr(0, tokenPos);
+                    token.erase(0, tokenPos + 1);
+                    res.headers.insert({lilToken, token});
+                }
+                resp.erase(0, pos + 2);
+            }
+            res.body = resp;
         }
         else {
-            res.status_code = ziapi::http::Code::kInternalServerError;
-            res.reason = "Error in the path or with the cgi.";    
+            throw std::runtime_error("error CreateProces");
         }
     }
     catch (const std::exception& e) {
