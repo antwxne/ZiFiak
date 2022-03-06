f69b95fb91c4e41438b987d3a9c7e92a20fb2f7c
clementleplusbo <clement1.ruat@epitech.eu>
2022-03-06 16:35:23 +0100

    edit: delete dylib.hpp

--- Sources/Server.cmake
+++ Sources/Server.cmake
@@ -4,7 +4,6 @@ set(SRC
         Sources/Debug/Debug.cpp Sources/Debug/Debug.hpp
         Sources/Exceptions/MyException.cpp Sources/Exceptions/MyException.hpp
         Sources/LoadLibs/LoadLibs.cpp Sources/LoadLibs/LoadLibs.hpp
-        Sources/dylib/dylib.hpp
         Sources/Server/ConfigParser.cpp Sources/Server/ConfigParser.hpp
         Sources/Queue/RequestQueue.cpp Sources/Queue/RequestQueue.hpp
         Sources/Queue/ResponseQueue.cpp Sources/Queue/ResponseQueue.hpp
